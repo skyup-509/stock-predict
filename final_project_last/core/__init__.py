c1091d5516eb4357612b77d2a91482440cff07c5
skyup-509 <parkcs0509@gmail.com>
2025-11-20 15:51:32 +0900

    Delete final_project_last directory

--- final_project_last/core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .preprocess import load_and_preprocess
-from .dataset import create_dataset
-from .trainer import train_model
-from .predictor import load_model, predict_delta
-from .strategy import compute_direction_accuracy, compute_return
--- final_project_last/kis/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import APP_KEY, APP_SECRET, ACCOUNT, ACCOUNT_TYPE
-from .token import get_token
-from .order import order_market_buy
