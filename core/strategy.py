b6e563cafec1c2af63430ef40fb60adc2fc4559b
skyup-509 <parkcs0509@gmail.com>
2025-11-24 14:49:08 +0900

    Delete core directory

--- core/strategy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def compute_direction_accuracy(real, pred):
-    return np.mean(np.sign(real) == np.sign(pred)) * 100
-
-def compute_return(real_price, pred_price):
-    return (pred_price[-1] - real_price[0]) / real_price[0] * 100
