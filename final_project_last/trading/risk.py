09fbdf3cf5c22779248699ab4911c6aed6cd94ca
skyup-509 <parkcs0509@gmail.com>
2025-11-25 03:06:30 +0900

    Delete final_project_last directory

--- final_project_last/trading/risk.py
+++
@@ -1,6 +0,0 @@
-def apply_risk(signal, current_position):
-    if signal == "BUY" and current_position == 0:
-        return "BUY"
-    if signal == "SELL" and current_position > 0:
-        return "SELL"
-    return "HOLD"
--- final_project_last/trading/signal.py
+++
@@ -1,6 +0,0 @@
-def generate_signal(pred_delta):
-    if pred_delta > 0:
-        return "BUY"
-    elif pred_delta < 0:
-        return "SELL"
-    return "HOLD"
